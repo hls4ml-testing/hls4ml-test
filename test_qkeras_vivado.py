2609e1595bccb735ceba0bd4f24a4d6e04da1832
Sarun Nun <sarun.nunt@gmail.com>
2020-10-18 13:14:58 +0700

    Chore: refactor repo

--- test_qkeras_vivado.py
+++ test_qkeras_vivado.py
@@ -1,40 +1,25 @@
-import pytest
-import random
-import string
-import numpy as np
-import hls4ml
-from qkeras import *
-from tensorflow.keras.layers import Input
 import os
 
+import pytest
+import string
+import numpy as np
+from qkeras import *
+from tensorflow.keras.layers import Input
 
-error=20
-
-def vivado_hls(model,input_shape,number,error):
-    input_data=np.random.randn(number,input_shape)
-
-    hls_model = hls4ml.converters.convert_from_keras_model(model,output_dir='temp',hls_config={ "default_precision":'ap_fixed<18,6>'})
-    hls_model.compile()
-    hls_model.write()
+import hls4ml
+from utils import vivado_hls
 
 
-    np.savetxt('./temp/tb_data/tb_input_features.dat',input_data)
+# xc7k70tl-fbv676-2L
+# xc7a12ti-csg325-1L
+# xc7a12tl-csg325-2L
+# xa7a35t-csg325-1I
+# xc7z020-clg484-2
+# xa7z010-clg225-1I
+# xc7s50-fgga484-2
+# xa7s6-cpga196-1I
 
-    prediction=model.predict(input_data)
-    np.savetxt('./temp/tb_data/tb_output_predictions.dat',prediction)
-
-    os.system('cd temp && vivado_hls -f build_prj.tcl "csim=1 synth=0 cosim=0 validation=0"')
-
-    hls_pred=t=np.loadtxt('./temp/tb_data/csim_results.log')
-
-    print(np.linalg.norm(prediction - hls_pred))
-    assert (np.linalg.norm(prediction - hls_pred) < error)
-
-    os.system("rm -rf temp")
-    os.system("rm temp.tar.gz")
-    os.system('rm -rf my-hls-test')
-    os.system("rm my-hls-test.tar.gz")
-
+error=15
 
 # ternary_tanh
 qactivation_list = ['quantized_relu', 'quantized_tanh', 'binary_tanh', 'quantized_bits']
@@ -79,3 +64,15 @@ def test_dense_stochastic(activation_kernel, activation_bias):
     # model.set_weights([w,b])
 
     vivado_hls(model,10,10,error)
+
+@pytest.mark.parametrize('activation', qactivation_list)
+def test_activation(activation):
+    test_input=np.random.randn(1,10)
+
+    x = x_in = Input(10)
+    x = QDense(10, kernel_quantizer='quantized_bits(3,0,1)', bias_quantizer='quantized_bits(3)', name='Qdense',)(x)
+    x = QActivation(activation)(x)
+
+    model = Model(inputs=x_in, outputs=x)
+    vivado_hls(model,10,10,error)
+
